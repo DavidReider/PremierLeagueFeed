3325e29639940acfabc363c4da100b4d8718e825
DavidReider <freakdj1997@yahoo.com>
2022-05-12 21:16:49 -0400

    Fetch Data and Output Cards

--- index.js
+++ index.js
@@ -11,8 +11,31 @@ const getJSON = async (url) => {
 console.log("Fetching data...");
 getJSON("https://premierleagueapi.herokuapp.com/news")
   .then((data) => {
-    console.log(data);
+    //console.log(data);
+
+    data.forEach((datum) => {
+      // create a new div for each news item
+      const card = createCard(datum.title, datum.url, datum.source);
+      document.body.appendChild(card);
+    });
   })
   .catch((error) => {
     console.error(error);
   });
+
+const createCard = (title, url, source) => {
+  const card = document.createElement("div");
+  const cardlink = document.createElement("a");
+  const cardtitle = document.createElement("h2");
+  const cardsource = document.createElement("h3");
+
+  card.appendChild(cardlink);
+  cardlink.appendChild(cardtitle);
+  cardlink.appendChild(cardsource);
+
+  cardtitle.innerText = title;
+  cardlink.href = url;
+  cardsource.innerText = source;
+
+  return card;
+};
